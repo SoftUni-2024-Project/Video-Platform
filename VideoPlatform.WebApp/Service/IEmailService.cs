f9c7ce5a14b0ab15920d1eb243ef4bf03dabfb24
GobbleWobble <120774541+GobbleWobble@users.noreply.github.com>
2024-02-21 10:34:15 +0200

    Add files via upload

---
+++ VideoPlatform.WebApp/Service/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using VideoPlatform.WebApp.Data.Entities;
+
+namespace VideoPlatform.WebApp.Services
+{
+    public interface IEmailService
+    {
+        void SendEmail(Message message);
+    }
+}
