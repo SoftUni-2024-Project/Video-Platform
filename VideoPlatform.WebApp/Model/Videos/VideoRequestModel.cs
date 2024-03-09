10913d20a957950e998b3c19247dd4701aa6aaa5
tsvetymanolova <ts.manlova23@gmail.com>
2024-03-09 13:17:19 +0200

    some video changes

--- VideoPlatform.WebApp/Model/Videos/VideoRequestModel.cs
+++ VideoPlatform.WebApp/Model/Videos/VideoRequestModel.cs
@@ -5,6 +5,7 @@ namespace VideoPlatform.WebApp.Model.Videos
     public class VideoRequestModel
     {
             public string Name { get; set; }
+            public string FilePath { get; set; }
             public string Description { get; set; }
             public Guid ChannelId { get; set; }
             public PrivacyEnum Privacy { get; set; }
--- VideoPlatform.WebApp/Model/Videos/VideoResponseModel.cs
+++ VideoPlatform.WebApp/Model/Videos/VideoResponseModel.cs
@@ -5,6 +5,7 @@ namespace VideoPlatform.WebApp.Model.Videos
     public class VideoResponseModel
     {
         public Guid Id { get; set; }
+        public string VideoUrl { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public Guid ChannelId { get; set; }
